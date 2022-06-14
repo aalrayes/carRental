ad35ea118a88d858734c89a81bd89cdc4bfdb27b
aalrayes <a.alrayes.404@gmail.com>
2022-06-14 17:23:40 +0300

    update structure

--- src/components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className=" mt-auto h-10 w-full  bg-red-400 text-center font-bold text-white">
-      Footer
-    </footer>
-  );
-}
--- src/components/layout/Spinner.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Spinner() {
-  return <div className="mx-auto h-screen w-screen">loading...</div>;
-}
